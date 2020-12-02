5ac2278181e96886dff84ce6270bc2c4203bdfd9
Aravindan22 <63189666+Aravindan22@users.noreply.github.com>
2020-12-02 09:46:52 +0530

    Added PDF Conversion

--- ResumeDateChanger.py
+++ ResumeDateChanger.py
@@ -1,5 +1,6 @@
-import docx
+import docx,os
 import datetime
+# from docx2pdf import convert
 doc = docx.Document("------- PATH TO RESUME DOCX FORMAT ONLY ---------")
 x=doc.paragraphs[0]
 for i in doc.paragraphs:
@@ -13,3 +14,8 @@ for i in doc.paragraphs:
     if(len(i.text.strip())==0 or len(i.text)==0):
         i.clear()
 doc.save('------- PATH TO RESUME DOCX FORMAT ONLY ---------')
+#Below Line is to using as script
+# convert("C:/Users/Pikachu/Downloads/Certs_and_res/Resume.docx","C:/Users/Pikachu/Downloads/Certs_and_res/Resume.pdf")
+#Below line is used for making application if used in script format it doesn't execute
+os.system("docx2pdf input.docx output.pdf")
+
